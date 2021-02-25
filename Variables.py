0495297b9eaf3cbe5d832cf4e5b4b1acacf90a58
Shirin Hk <shirinhk@Shirins-MacBook-Pro.local>
2021-02-24 22:52:01 -0500

    writing our first Python script

---
+++ Variables.py
@@ -0,0 +1,17 @@
+# This is a comment
+# This is a print method.
+
+print("This is my very first Python script!")
+
+name = "Shirin"
+age = 30
+haircolor = "brown"
+eyecolor= "hazel"
+
+cars = ["volvo", "Toyota", "Fiesta"]
+
+print ("My name is " + name)
+name = input("What is your name?")
+print("My name is now "+ name)
+
+print("my age is "+str(age))
